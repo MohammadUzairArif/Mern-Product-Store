54920cc731a4f9a48b71b4b2896db6efed91db58
user.name like : "uzairarif28@gmail.com <uzairarif28@gmail.com>
2025-04-24 21:56:21 +0500

    first commit

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+dotenv.config();
+const app = express();
+
+app.post('/products', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.listen(8000,()=>{
+    connectDB()
+    console.log("Server is running on port 8000")
+})
+
+//
