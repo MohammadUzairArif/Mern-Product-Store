e8ccd90c8734913c07f3eb31d9755334a24467d6
user.name like : "uzairarif28@gmail.com <uzairarif28@gmail.com>
2025-04-26 00:43:35 +0500

    first commit

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { Container, Flex } from '@chakra-ui/react'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <Container maxW={"1140px"} px={4}>
+        <Flex h={"16"} alignItems={"center"} justifyContent={"space-between"}>
+
+        </Flex>
+    </Container>
+  )
+}
+
+export default Navbar
